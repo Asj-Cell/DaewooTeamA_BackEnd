3ab1cf52b493967d0e5e64a7c7d204789cd7d610
JHS5020 <goals00002@naver.com>
2025-11-03 21:27:44 +0900

    예외처리 관련 조금씩 진행중
    exception에 CommonExceptionTemplate 상속을 런타임예외로 변경

--- backend/src/main/java/com/example/backend/common/exception/MemberException.java
+++ backend/src/main/java/com/example/backend/common/exception/MemberException.java
@@ -2,7 +2,11 @@ package com.example.backend.common.exception;
 
 public enum MemberException {
 
-    NOT_EXIST_MEMBER_ID(400400, "그런 아이디 없음");
+    NOT_EXIST_MEMBER_ID(400400, "그런 아이디 없음"),
+
+    USER_NOT_FOUND(404400, "해당 ID의 사용자를 찾을 수 없습니다."),
+
+    LOGIN_REQUIRED(401401, "로그인이 필요합니다. (토큰 없음)");
 
     private CommonExceptionTemplate t;
 
